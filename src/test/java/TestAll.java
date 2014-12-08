72cf19d4b046c9756da9a4ef3bff5310d0aea8e0
Resul42 <Resul-g_93@hotmail.com>
2014-12-08 23:30:18 +0100

    everything

---
+++ src/test/java/TestAll.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class TestAll {
+
+	@Test
+	public void test() {
+		Meth56 methode = new Meth56();
+		String resultString = methode.addStrings("ik", "heet", "Resul");
+		assertEquals("ikheetResul", resultString);
+		int resultDevide = methode.devide(60, 10);
+		assertEquals(6, resultDevide);
+		int resultCount = methode.count(66, 77, 4, 6);
+		assertEquals(153, resultCount);
+		int resultCalculate = methode.calculate(8, 6);
+		assertEquals(48, resultCalculate);
+	}
+}
