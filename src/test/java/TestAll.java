bb7435539deadeb0d53053779b89f7df4c191ad4
Resul42 <Resul-g_93@hotmail.com>
2014-12-09 09:31:15 +0100

    Klassen netjes gezet

--- src/test/java/TestAll.java
+++ src/test/java/TestAll.java
@@ -4,16 +4,25 @@ import org.junit.Test;
 
 
 public class TestAll {
-
+	Meth56 methode = new Meth56();
 	@Test
-	public void test() {
-		Meth56 methode = new Meth56();
+	public void testString() {
+		
 		String resultString = methode.addStrings("ik", "heet", "Resul");
 		assertEquals("ikheetResul", resultString);
+	}
+	@Test
+	public void testDevide(){
 		int resultDevide = methode.devide(60, 10);
 		assertEquals(6, resultDevide);
+	}
+	@Test
+	public void testCount(){
 		int resultCount = methode.count(66, 77, 4, 6);
-		assertEquals(153, resultCount);
+		assertEquals(153, resultCount);	
+	}
+	@Test
+	public void testCalculate(){
 		int resultCalculate = methode.calculate(8, 6);
 		assertEquals(48, resultCalculate);
 	}
